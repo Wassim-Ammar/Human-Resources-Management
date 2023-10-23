9ef36c53a50090db5ea2af36bcf854e7532ebee9
wess ammar <wesswessest@gmail.com>
2023-10-23 20:08:49 +0100

    Modify readme file

--- src/routes/api.php
+++ src/routes/api.php
@@ -11,21 +11,6 @@ use App\Http\Controllers\VaccationController;
 
 
 
-
-
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
-
-// Route::middleware('auth:sanctum')
-//     ->
 Route::middleware('auth:sanctum')->controller(AuthController::class)
     ->group(function () {
         Route::get('me', 'me');
