407c41891af134b318cdf16a9602c858762a8a6f
wess ammar <wesswessest@gmail.com>
2023-10-23 19:22:58 +0100

    Modify seeders

--- src/database/seeders/DatabaseSeeder.php
+++ src/database/seeders/DatabaseSeeder.php
@@ -14,17 +14,17 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(3)->create();
-        // \App\Models\Vaccation::factory(1)->create([
-        //     'user_id' => 1,
-        // ]);
-        // \App\Models\Announcement::factory(3)->create();
-        // \App\Models\Meeting::factory(3)->create();
-        //ToDoItem::factory(5)->create();
-        \App\Models\User::factory()->create(['email' => "elbehi@gmail.com"]);
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        \App\Models\User::factory(3)->create();
+        \App\Models\Vaccation::factory(1)->create([
+            'user_id' => 1,
+        ]);
+        \App\Models\Announcement::factory(3)->create();
+        \App\Models\Meeting::factory(3)->create();
+        ToDoItem::factory(5)->create();
+
+        \App\Models\User::factory()->create([
+            'name' => 'Test User',
+            'email' => 'test@example.com',
+        ]);
     }
 }
